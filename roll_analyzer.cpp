1cd9d38265cd4d51dce7ae1e136686dccdbc9164
Sebastian Amara <sqamara@ucsd.edu>
2015-07-23 00:34:26 -0700

    developed two die info

--- roll_analyzer.cpp
+++ roll_analyzer.cpp
@@ -16,8 +16,12 @@
 #include <iostream>
 #define PURSUE_PERCENT .50
 
-
 using namespace std;
+#define NUMBER_OF_SIDES 6
+
+int roll() {
+	return rand() % NUMBER_OF_SIDES + 1;
+}
 
 struct bestChoice {
 	double probability;
@@ -63,7 +67,71 @@ void print_two_probabilities() {
 	cout << endl;
 }
 
+void tabled_two_die() {
+	int die1_by_die2_by_rerollmin[6][6][6];
+	// prob of any individual element in array is 1/6^3
+	// values between 2 and 12 inclusive
+	for (int die1 = 0; die1 < 6; die1++) {
+		for (int die2 = 0; die2 < 6; die2++) {
+			for (int rerollmin = 0; rerollmin < 6; rerollmin++) {
+				die1_by_die2_by_rerollmin[die1][die2][rerollmin] = max((die1+1), (die2+1)) + (rerollmin+1);
+			}
+		}
+	}
+
+	for (int rerollmin = 0; rerollmin < 6; rerollmin++) {
+		for (int die1 = 0; die1 < 6; die1++) {
+			for (int die2 = 0; die2 < 6; die2++) {
+				printf("%2d ",die1_by_die2_by_rerollmin[die1][die2][rerollmin]);
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
+	int count[12];
+	for (int i = 0; i<12; i++)
+		count[i] = 0;
+	for (int die1 = 0; die1 < 6; die1++) {
+		for (int die2 = 0; die2 < 6; die2++) {
+			for (int rerollmin = 0; rerollmin < 6; rerollmin++)
+				count[ die1_by_die2_by_rerollmin[die1][die2][rerollmin]-1]++;
+		}
+	}
+
+	for (int i = 0; i<12; i++)
+		printf("%lf\n", double(count[i])/(6*6*6));
+		//printf("%d: %d: %lf\n", i+1, count[i], double(count[i])/(6*6*6));
+
+
+
+	//simulate
+	int simulate[12];
+	int simulate_with_reroll[12];
+
+	for (int i = 0; i<12; i++) {
+		simulate[i] = 0;
+		simulate_with_reroll[i] = 0;
+	}
+	int cases = 100;
+	for (int i = 0; i<cases; i++) {
+		int roll1 = roll();
+		int roll2 = roll();
+		simulate[roll1+roll2-1]++;
+		simulate_with_reroll[max(roll1, roll2) + roll()-1]++;
+	}
+	for (int i = 0; i<12; i++)
+		printf("%lf,\n", double(simulate[i])/cases);
+
+}
+
+
 int main() {
+	srand(time(NULL));
+
 	//cout << analyze_one()->number_to_pursue << endl;
+	tabled_two_die();
+
 	print_two_probabilities();
+
  }
